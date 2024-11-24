9b8b555c7720ee3462d86c607a71213f7ee3658c
Prashanth Pratapagiri <>
2024-11-24 20:47:20 +0000

    Initial commit

---
+++ apps/ev-api1/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'ev-api1',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ev-api1',
+};
