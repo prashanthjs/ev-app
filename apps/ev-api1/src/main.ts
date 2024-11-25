bc2f81e848be6dd91cad0829dffcc9166177aa34
Prashanth Pratapagiri <>
2024-11-25 00:06:06 +0000

    Runs the tests and generates the documentation.

--- apps/ev-api1/src/main.ts
+++ apps/ev-api1/src/main.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { serve } from '@hono/node-server';
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 4200;
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
