72a82ddfc3cff3adf97b9e5943e16f2714e139d6
Prashanth Pratapagiri <>
2024-11-26 08:53:30 +0000

    EVFrontend description is updated

--- apps/ev-frontend1/src/app/app.tsx
+++ apps/ev-frontend1/src/app/app.tsx
@@ -18,7 +18,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          EV Frontend1 is running
+          EV Frontend1 is running!!
         </p>
       </div>
     </div>
